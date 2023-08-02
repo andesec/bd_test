552f6557bc57b707036ea02fbd5df3b1b650c74b
Nasir Abdul Karim <nkarim@timexperts.com.pk>
2023-08-02 12:09:44 +0500

    initial commit

---
+++ src/main/java/com/smallworld/data/Transaction.java
@@ -0,0 +1,25 @@
+package com.smallworld.data;
+
+import java.math.BigDecimal;
+
+public class Transaction {
+    // Represent your transaction data here.
+
+  int mtn;
+
+  BigDecimal amount;
+
+  String senderFullName;
+
+  int senderAge;
+
+  String beneficiaryFullName;
+
+  int beneficiaryAge;
+
+  int issueId;
+
+  Boolean issueSolved;
+
+  String issueMessage;
+}
